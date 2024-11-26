84fef49efe6477f64623f8e19d74f5fdd3806d09
Felipe Oliveira <felipezx303@gmail.com>
2024-11-26 10:55:54 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Livro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string ISBN { get; set; }
+    public string Genero { get; set; }
+    public bool Disponivel { get; set; }
+
+    public Livro(string titulo, string autor, string isbn, string genero)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        ISBN = isbn;
+        Genero = genero;
+
+    }
+}
+
+public class Usuario
+{
+    public string Nome { get; set; }
+}
